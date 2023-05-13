944703d44433effb058aa8cb0403dcfdd4c3e3c0
Felipe Maximiliano da Rosa <fmo19@bskyb.com>
2023-05-13 23:31:30 +0100

    chore: adding an user on db

--- src/database/db.sql
+++ src/database/db.sql
@@ -4,7 +4,9 @@ USE `fastfoodius`;
 CREATE TABLE IF NOT EXISTS `fastfoodius`.`users` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `nome` VARCHAR(255) NOT NULL,
-  `cpf` VARCHAR(11) NOT NULL,
+  `email` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`id`)
 )
 ENGINE = InnoDB;
+
+INSERT INTO `fastfoodius`.`users` (name, email) VALUES ('felipe', 'felipemax.suporteti@gmail.com');
