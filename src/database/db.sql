72c307534421b5334a569077ebb207bd8a118c63
Felipe Maximiliano da Rosa <fmo19@bskyb.com>
2023-05-13 23:11:12 +0100

    chore: database start

---
+++ src/database/db.sql
@@ -0,0 +1,10 @@
+CREATE SCHEMA IF NOT EXISTS `fastfoodius` DEFAULT CHARACTER SET utf8;
+USE `fastfoodius`;
+
+CREATE TABLE IF NOT EXISTS `fastfoodius`.`users` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(255) NOT NULL,
+  `cpf` VARCHAR(11) NOT NULL,
+  PRIMARY KEY (`id`)
+)
+ENGINE = InnoDB;
