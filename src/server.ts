e32563cef330e8d7c9d0f3b0ffaa6c7934c5acdb
Felipe Maximiliano da Rosa <fmo19@bskyb.com>
2023-05-15 12:31:04 +0100

    chore: connecting to local database

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,13 @@
 import express from 'express';
+import mysql from 'mysql2';
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'fastfoodius_user',
+  password: 'fastfoodius_pass',
+  database: 'fastfoodius',
+});
 
 // Constants
 const PORT = 8080;
@@ -11,4 +20,12 @@ app.get('/', (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Running on PORT:${PORT}`);
+});
+
+connection.connect((error) => {
+  if (error) {
+    return console.error(`Error: ${error}`);
+  }
+
+  console.log('Connected to MySQL Server!');
 });
